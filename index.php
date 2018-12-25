83bed5a8ffcb7253c4342ced2f1b6da7180706e9
sanfren <sanfren@gmail.com>
2018-12-25 12:26:52 -0200

    Gitignore adicionado

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>  
+
+<html>  
+   <head> 
+      <meta charset = "utf-8"> 
+      <title>Agenda de estudos</title> 
+      <link href="style.css" rel="stylesheet">
+   </head> 
+  
+   <body> 
+      <header role = "banner"> 
+         <h1>Agenda de estudos</h1> 
+         <p>Organização de conteúdos</p>          
+      </header> 
+   
+      <!-- Menus ou links ou imagens (principais) -->
+      <nav> 
+         <ul> 
+            <li><a href = "https://www.sanfren.com/agenda">Agenda</a></li> 
+            <li><a href = "https://www.sanfren.com/conteudos">Conteúdos</a></li> 
+            <li><a href = "https://www.sanfren.com/topicos">Tópicos</a></li> 
+            JavaScript Tutorial</a></li> 
+         </ul> 
+      </nav> 
+   
+      <article> 
+         <section> 
+            <p>Once article can have multiple sections</p>
+         </section> 
+      </article> 
+   
+      <aside> 
+         <p>This is  aside part of the web page</p> 
+      </aside> 
+   
+      <footer> 
+         <p>Created by <a href = "https://sanfren.com/">Agenda Prática</a></p> 
+      </footer> 
+   
+   </body> 
+</html> 
